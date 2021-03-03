b903bf6d6f9dd5b6d0199a9513fc6fa7d82786cb
Joel Holm <jdholm@iastate.edu>
2021-03-03 15:09:54 -0600

    playing with the build

--- UI/src/app/nav/nav.module.ts
+++ UI/src/app/nav/nav.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { MaterialModule } from '../material/material.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [NavComponent],
@@ -13,6 +14,7 @@ import { AppRoutingModule } from '../app-routing.module';
   ],
   exports: [
     NavComponent
-  ]
+  ],
+  providers: []
 })
 export class NavModule { }
