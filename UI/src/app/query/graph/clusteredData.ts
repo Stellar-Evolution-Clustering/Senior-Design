7f5fed02cb514de8102739155e8358ab78e4591b
Joel Holm <jdholm@iastate.edu>
2021-03-16 23:08:13 -0500

    Added a class to organize the graph component's data, ClusteredData

---
+++ UI/src/app/query/graph/clusteredData.ts
@@ -0,0 +1,70 @@
+
+
+/**
+* ClusteredData
+*
+* A class to organize data returned from API to be used in graph component
+*/
+export class ClusteredData {
+
+  numClusters: number;
+  attributes: string[];
+  jsonData: any;
+
+
+  constructor(jsonData: any) {
+    this.jsonData = jsonData;
+
+    let attrs = Object.keys(jsonData[0]["coords"]);
+    this.attributes = new Array(attrs.length);
+    for(let i = 0; i < attrs.length; i++) {
+      this.attributes[i] = attrs[i];
+    }
+
+    //TODO: extract num clusters from jsonData -> numClusters
+    
+  }
+
+  getAllAttr(): string[] {
+    return this.attributes;
+  }
+
+  /**
+  *
+  * Generates all clusters from the stored data
+  *
+  */
+  generate2DGraphData(attr1: string, attr2: string): any {
+    //TODO: format the data of this class so that it can be plugged into a plot.ly 3d graph
+
+  }
+
+  /**
+  *
+  * Generates a single cluster from the stored data
+  *
+  */
+  getSelectiveCluster2DData(clusterNums: number[], attr1: string, attr2: string): any {
+
+  }
+
+  /**
+  *
+  * Generates all clusters from the stored data
+  *
+  */
+  generate3DGraphData(attr1: string, attr2: string, attr3: string): any {
+    //TODO: format the data of this class so that it can be plugged into a plot.ly 3d graph
+
+  }
+
+  /**
+  *
+  * Generates a single cluster from the stored data
+  *
+  */
+  getSelectiveCluster3DData(clusterNum: number[], attr1: string, attr2: string, attr3: string): any {
+
+  }
+
+}
--- UI/src/app/query/graph/graph.component.ts
+++ UI/src/app/query/graph/graph.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { QueryService } from '../../api/query.service'
+import { QueryService } from '../../api/query.service';
+import { ClusteredData } from './clusteredData';
 
 import * as Plotly from 'plotly.js/dist/plotly.js';
 
 // PlotlyModule.plotlyjs = Plotly;
+
+//TODO: Loading screen during query, or hide 2d graph while loading
 
 @Component({
   selector: 'app-graph',
@@ -17,6 +20,7 @@ export class GraphComponent implements OnInit {
   selectedCluster: string;
 
   private data3D: any;
+  private clusteredData: ClusteredData;
 
   constructor(
     private queryService: QueryService,
@@ -25,12 +29,11 @@ export class GraphComponent implements OnInit {
   ngOnInit(): void {
     this.selectedCluster = "all";
     console.log(this.selectedCluster);
-    this.data3D = this.makeRandomData3D();
+    this.data3D = null;
     this.graph = {
-          data: this.makeRandomData(),
+          data: null,
           layout: {width: 640, height: 480, title: 'A 2D graph'}
     };
-    this.getBackendDataTest();
     this.graph3D = {
           data: this.data3D,
           layout: {
@@ -79,6 +82,7 @@ export class GraphComponent implements OnInit {
             width: 640
           }
     };
+    this.getBackendDataTest();
   }
 
   zoomCluster(): void {
@@ -102,7 +106,7 @@ export class GraphComponent implements OnInit {
       console.log(response);
 
       var data = [];
-      var colors: string[] = ['red','blue','yellow'];
+      var colors: string[] = ['red','blue','green'];
       for( let i = 0; i < 3; i ++ ){
         data.push(
           { x: [], y: [], z: [], type: 'scatter3d', mode: 'markers', marker: {color: colors[i], size: 2}, name: `Cluster ${i + 1}`}
@@ -115,76 +119,10 @@ export class GraphComponent implements OnInit {
         data[clusterNum].z.push(response[j]["coords"]["porb"]);
       }
 
+      this.clusteredData = new ClusteredData(response);
       this.data3D = data;
       this.graph3D["data"] = data;
     });
-  }
-
-  makeRandomData(): any[] {
-    //idk, somewhat cluster like data ...
-    var data = [];
-
-    var width: number = 2;
-    var height: number = 3;
-    var size: number = 30;
-
-    var centers_x: number[] = [2, 3, 9];
-    var centers_y: number[] = [2, 19, 7];
-    var colors: string[] = ['red','blue','yellow'];
-    for( let i = 0; i < 3; i++ ){
-      data.push({ x: [], y: [], type: 'scatter', mode: 'markers', marker: {color: colors[i]}, name: `Cluster ${i + 1}`} );
-      for( let j = 0; j < size; j++ ){
-        data[i].x.push(
-          centers_x[i] + ((this.randn_bm() - 0.5) * width)
-        );
-        data[i].y.push(
-          centers_y[i] + ((this.randn_bm() - 0.5) * height)
-        );
-      }
-    }
-
-    return data;
-  }
-
-  makeRandomData3D(): any[] {
-    //idk, somewhat cluster like data ...
-    var data = [];
-
-    var width: number = 2;
-    var height: number = 3;
-    var depth: number = 2;
-    var size: number = 30;
-
-    var centers_x: number[] = [2, 3, 9];
-    var centers_y: number[] = [2, 19, 7];
-    var centers_z: number[] = [-2, 5, 1];
-    var colors: string[] = ['red','blue','yellow'];
-    for( let i = 0; i < 3; i ++ ){
-      data.push(
-        { x: [], y: [], z: [], type: 'scatter3d', mode: 'markers', marker: {color: colors[i], size: 2}, name: `Cluster ${i + 1}`}
-      );
-      for( let j = 0; j < size; j++ ){
-        var rand_x = centers_x[i] + (this.randn_bm() - 0.5) * width;
-        var rand_y = centers_y[i] + (this.randn_bm() - 0.5) * height;
-        var rand_z = centers_z[i] + (this.randn_bm() - 0.5) * depth;
-
-        data[i].x.push(rand_x);
-        data[i].y.push(rand_y);
-        data[i].z.push(rand_z);
-      }
-    }
-    console.log(data);
-
-    return data;
-  }
-
-  // Standard Normal variate using Box-Muller transform.
-  // https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
-  randn_bm(): number {
-    var u = 0, v = 0;
-    while(u === 0) u = Math.random();
-    while(v === 0) v = Math.random();
-    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
   }
 
 }
