5ac9f14ba6c6013ed385ce1ed7d118a6965fd527
adamcorp <adamcorp@iastate.edu>
2021-03-23 18:12:01 -0500

    Change to vertical stepper

--- UI/src/app/api/api.module.ts
+++ UI/src/app/api/api.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VariablesService } from './variables.service';
-import { ClusteringService } from './clustering.service';
-import { DatabaseService } from './database.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [VariablesService, ClusteringService, DatabaseService],
+  providers: [VariablesService],
 })
 export class ApiModule {}
